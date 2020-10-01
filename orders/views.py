184e125733cf10df750a06b36a496842dd928be9
volodymyrb <volodymyrb@fornova.net>
2020-10-01 23:12:06 +0300

    orders

---
+++ orders/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+from .models import Order, OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+
+def order_create(request: HttpRequest):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order: Order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity'],
+                )
+            cart.clear()
+            context = {
+                'order': order,
+            }
+
+            return render(request, 'orders/order/created.html', context)
+    else:
+        form = OrderCreateForm()
+
+    context = {
+        'cart': cart,
+        'form': form,
+    }
+
+    return render(request, 'orders/order/create.html', context)
